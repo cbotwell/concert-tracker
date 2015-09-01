d396582b68ce3063de68476f5c1cbe0e3cf6db05
Corbin Otwell <cbotwell@gmail.com>
2015-09-01 15:24:31 -0500

    adds torri, routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,25 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('index', {path: '/'});
+
+  this.route('login');
+  this.route('signup');
+
+  this.route('venues', function() {
+    this.route('index', {path: '/'});
+    this.route('show');
+  });
+
+  this.route('concerts', function() {
+    this.route('index', {path: '/'});
+    this.route('show');
+  });
+
+  this.route('dashboard');
+  this.route('profile');
+
+  this.route('post', {path: 'post/:id'});
 });
 
 export default Router;
