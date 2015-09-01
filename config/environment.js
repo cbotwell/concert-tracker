7c82c8ad6bcf4e5c1faa58b0f8c11555330fd6e7
Corbin Otwell <cbotwell@gmail.com>
2015-09-01 14:26:13 -0500

    brings in emberfire, neat, bourbon, reset

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'concert-tracker',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://co-concert-tracker.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
