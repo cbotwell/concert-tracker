8d14497099d434166ece2a9ff002d75e42fec02e
Corbin Otwell <cbotwell@gmail.com>
2015-09-04 09:02:25 -0500

    adds dashboard template

--- app/dashboard/route.js
+++ app/dashboard/route.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model: function() {
+    return Ember.RSVP.hash({
+      concerts: this.store.findAll('concert'),
+      user: this.get('session.currentUser'),
+    });
+  },
 });
