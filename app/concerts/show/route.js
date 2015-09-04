87692f3b656c953ac30154ad1e9e4769b2fbc8f5
Corbin Otwell <cbotwell@gmail.com>
2015-09-04 14:01:21 -0500

    user can save a concert

--- app/concerts/show/route.js
+++ app/concerts/show/route.js
@@ -4,16 +4,17 @@ export default Ember.Route.extend({
   model: function(params) {
     return Ember.RSVP.hash({
       concert: this.store.findRecord('concert', params.concert_id),
-      user: this.get('session.currentUser'),
+      user: this.get('session').getCurrentUser(),
     });
   },
   actions: {
     saveConcert: function(concert) {
-      var user = this.get('session.currentUser');
-      user.savedConcerts.addObject(concert);
-      user.save();
-      concert.incrementProperty('rsvps', 1);
-      concert.save();
-    }
+      this.get('session').getCurrentUser().then((user) => {
+        user.get('savedConcerts').addObject(concert);
+        user.save();
+        concert.incrementProperty('rsvps', 1);
+        concert.save();
+      });
+    },
   },
 });
