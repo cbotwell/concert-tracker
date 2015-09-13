ced580d685b5ba5a2e14db1d6be3332400e5e513
Corbin Otwell <cbotwell@gmail.com>
2015-09-13 17:08:17 -0500

    changes to .filter for upcoming and past concerts on profile

--- app/components/profile-saved-concerts/component.js
+++ app/components/profile-saved-concerts/component.js
@@ -4,25 +4,18 @@ import moment from 'moment';
 export default Ember.Component.extend({
   concerts: [],
 
-  savedConcertsByTime: Ember.computed('concerts', 'concerts.@each.date', function() {
-    return this.get('concerts').reduce((prev, current) => {
-        var upcoming = prev.findBy('name', 'Upcoming');
-        var past = prev.findBy('name', 'Past');
-        var date = current.get('date');
-        debugger;
-        if (date > moment()) {
-          upcoming.concerts.push(current);
-        } else {
-          past.concerts.push(current);
-        }
-        return prev;
-      }, [{
-            name: 'Upcoming',
-            concerts: []
-          },
-          {
-            name: 'Past',
-            concerts: []
-          }]);
+  upcomingConcerts: Ember.computed.filter('concerts', function(concert) {
+    var date = concert.get('date');
+    if (date > moment()) {
+      return concert;
+    }
   }),
+
+  pastConcerts: Ember.computed.filter('concerts', function(concert) {
+    var date = concert.get('date');
+    if (date < moment()) {
+      return concert;
+    }
+  }),
+
 });
