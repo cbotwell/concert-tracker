5943da27ade9351bab6783ab67a50b0ca5781e8e
Corbin Otwell <cbotwell@gmail.com>
2015-09-13 17:16:04 -0500

    changes this week and upcoming to use .filter

--- app/components/this-week/component.js
+++ app/components/this-week/component.js
@@ -4,13 +4,11 @@ import moment from 'moment';
 export default Ember.Component.extend({
   concerts: [],
 
-  thisWeek: Ember.computed('concerts', 'concert.@each.date', function() {
-    return this.get('concerts').reduce((prev, current) => {
-      var date = current.get('date');
-      if (date > moment() && date < moment().add(7, 'days')) {
-        prev.push(current);
-      }
-      return prev;
-    }, []);
+  thisWeek: Ember.computed.filter('concerts', function(concert) {
+    var date = concert.get('date');
+    if (date > moment() && date < moment().add(7, 'days')) {
+        return concert;
+    }
   }),
+
 });
--- app/components/upcoming-concerts/component.js
+++ app/components/upcoming-concerts/component.js
@@ -4,13 +4,11 @@ import moment from 'moment';
 export default Ember.Component.extend({
   concerts: [],
 
-  upcomingConcerts: Ember.computed('concerts', 'concert.@each.date', function() {
-    return this.get('concerts').reduce((prev, current) => {
-      var date = current.get('date');
-      if (date > moment()) {
-        prev.push(current);
-      }
-      return prev;
-    }, []);
+  upcomingConcerts: Ember.computed.filter('concerts', function(concert) {
+    var date = concert.get('date');
+    if (date > moment()) {
+      return concert;
+    }
   }),
+
 });
