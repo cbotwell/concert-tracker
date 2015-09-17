424e92e83d45253ae7be509f781c0858855d9a69
Corbin Otwell <cbotwell@gmail.com>
2015-09-17 12:53:27 -0500

    fixes profile venue links and adds in toggleFav

--- app/profile/route.js
+++ app/profile/route.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import ToggleFav from 'concert-tracker/mixins/toggle-fav';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ToggleFav, {
   model: function() {
     return this.get('session').getCurrentUser();
   },
